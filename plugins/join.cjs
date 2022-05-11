9831ba4ca9ed0a1b423496e70d7b9c9a10ff7d3f
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-05-11 22:00:32 +0530

    Update join.cjs

--- plugins/join.cjs
+++ plugins/join.cjs
@@ -9,7 +9,7 @@ let handler = async (m, {
 	if (!code) throw 'Link invalid'
 	let res = await conn.groupAcceptInvite(code)
 	expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-	m.reply(`Berhasil join grup ${res}${expired ? ` selama ${expired} hari` : ''}`)
+	m.reply(`Berhasil join grup ${res}${expired ? ` During ${expired} day` : ''}`)
 	let chats = global.db.data.chats[res]
 	if (!chats) chats = global.db.data.chats[res] = {}
 	if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
